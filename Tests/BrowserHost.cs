ccf2f5b0ef0496238d082d0884727a39abacfbff
Ilian Kostov <ilian.kostov@gmail.com>
2017-05-09 21:55:22 +0300

    Update selenium gecko driver

--- Tests/BrowserHost.cs
+++ Tests/BrowserHost.cs
@@ -5,6 +5,7 @@
     public static class BrowserHost
     {
         public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl = @"http://localhost:60634/Article/List";
 
         static BrowserHost()
         {
